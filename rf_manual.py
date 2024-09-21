a9563c213391229f3a440d55925fc892e91d2b2d
kavmal7 <kavyamal74@gmail.com>
2024-09-21 20:22:21 +0200

    Updated evaluation notebook, set for running

--- rf_manual.py
+++ rf_manual.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import decision_tree_id3
+import decision_tree_id3 as my_dt
 
 class RandomForestManual:
     def __init__(self, n_trees, max_features=None, impurity_measure='entropy'):
@@ -17,7 +17,7 @@ class RandomForestManual:
     def fit(self, X, y):
         for _ in range(self.n_trees):
             X_sample, y_sample = self.bootstrap(X, y)
-            tree = decision_tree_id3.id3(X_sample, y_sample, self.impurity_measure, self.max_features)
+            tree = my_dt.id3(X_sample, y_sample, self.impurity_measure, self.max_features)
             self.trees.append(tree)
 
     def predict(self, X):
@@ -26,7 +26,7 @@ class RandomForestManual:
         return final_preds
     
     def predict_tree(self, tree, X):
-        return np.array([decision_tree_id3.predict(x, tree) for x in X])
+        return np.array([my_dt.predict(x, tree) for x in X])
     
     def majority_vote(self, preds):
         vals, counts = np.unique(preds, return_counts=True)
